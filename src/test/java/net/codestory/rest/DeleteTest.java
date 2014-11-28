7300961c72f892e3decb70d2208263e3ee31d302
David Gageot <david@gageot.net>
2014-11-28 10:50:50 +0100

    Fix test name

--- src/test/java/net/codestory/rest/DeleteTest.java
+++ src/test/java/net/codestory/rest/DeleteTest.java
@@ -20,7 +20,7 @@ import org.junit.Test;
 
 public class DeleteTest extends AbstractTest {
   @Test
-  public void delete_empty() {
+  public void delete() {
     server.configure(routes -> routes
         .delete("/", () -> Payload.ok())
     );
